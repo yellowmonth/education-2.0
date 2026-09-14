11e7ab1073ad46ce990b24b7443c40a057f660a0
yellowmonth <vadimchernousov2005@gmail.com>
2026-09-14 17:57:49 +0300

    Блок 1 Задание 4

---
+++ Program4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr0gram4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину ребра куба: ");
+            int dlina = int.Parse(Console.ReadLine());
+
+            int volume = dlina * dlina * dlina;
+            Console.WriteLine($"Объём куба:{volume}  ");
+
+            int square = dlina * dlina * 6;
+            Console.WriteLine($"Площадь куба: {square}");
+        }
+    }
+}
