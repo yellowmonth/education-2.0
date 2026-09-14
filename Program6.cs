de130c874d00558cae34188698639d14a6f45db2
yellowmonth <vadimchernousov2005@gmail.com>
2026-09-14 18:22:18 +0300

    Блок 1 Задание 6

---
+++ Program6.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение a: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение b: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Исходные значения: a = {a}, b = {b}");
+
+            //  Способ первый: С использованием третьей переменной 
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine($"После обмена через третью переменную: a = {a}, b = {b}");
+
+            // Способ второй: арифметический без третьей переменной 
+            a = a + b;  
+            b = a - b;  
+            a = a - b;
+
+            Console.WriteLine($"После обмена без третьей переменно: a = {a}, b = {b}");
+
+        }
+    }
+}
