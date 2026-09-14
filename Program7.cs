66bab17e3c637d6b10ee3886ba3e70983842cee5
yellowmonth <vadimchernousov2005@gmail.com>
2026-09-14 18:28:55 +0300

    Блок 1 Задание 7

---
+++ Program7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            double pervoe = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            double vtoroe = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите третье число: ");
+            double third = double.Parse(Console.ReadLine());
+
+            double avg = (pervoe + vtoroe + third) / 3;
+
+            Console.WriteLine($"Среднее арифметическое: {avg:F2}");
+        }
+    }
+}
