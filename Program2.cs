1514334b516dde0e7eefd97b6ea6f6911f62d590
yellowmonth <vadimchernousov2005@gmail.com>
2026-09-14 17:37:07 +0300

    1 блок 2 задание

--- Program2.cs
+++ Program2.cs
@@ -9,8 +9,7 @@ namespace ConsoleApp2
     internal class Program
     {
         static void Main(string[] args)
-        {
-            //Запросите у пользователя два целых числа. Выведите их сумму, разность, произведение, а также целочисленное частное и остаток от деления.
+        { 
 
             Console.Write("Введите первое целое число: ");
             int name1 = int.Parse(Console.ReadLine());
@@ -18,29 +17,22 @@ namespace ConsoleApp2
             Console.Write("Введите второе целое число: ");
             int name2 = int.Parse(Console.ReadLine());
 
-            double sum = name1 + name2;
+            int sum = name1 + name2;
 
             Console.WriteLine($"Сумма: {sum}");
 
-            double ruz = name1 / name2;
+            int ruz = name1 - name2;
             Console.WriteLine($"Разность: {ruz}");
 
-            double proz = name1 * name2;
+            int proz = name1 * name2;
             Console.WriteLine($"Произведение: {proz}");
 
-            // Остаток от деления
-
-            double ost = name1 % name2;
+            int ost = name1 % name2;
             Console.WriteLine($"Остаток от деления: {ost}");
-            // осталось допилить целочисленное частное ????
 
-            // Вычисление и вывод целочисленного частного
             int quotient = name1 / name2;
-            Console.WriteLine($"Целочисленное частное: {name1} / {name2} = {quotient}");
+            Console.WriteLine($"Целочисленное частное: {quotient}");
 
-
-            // 16 и 18 строка должно быть  double name{} = double.parse
-            // 38 строка int quiotinent доделать
         }
     }
 }
